ad7e781c82d2b3c5727fb6f75d262374b03d1ab4
JohnSen0 <volars11@gmail.com>
2025-07-02 19:44:52 +0300

    Практика 7

--- f-unit6/f-unit6.1/src/main/java/org/example/ShellInvoker.java
+++ f-unit6/f-unit6.1/src/main/java/org/example/ShellInvoker.java
@@ -29,27 +29,6 @@ public class ShellInvoker {
     }
 
     private void registerCommandsFromPackage(String packageName) {
-        try {
-            ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            URL packageURL = loader.getResource(packageName.replace('.', '/'));
 
-            File packageDir = new File(packageURL.getFile());
-            for (File file : packageDir.listFiles()) {
-                if (file.getName().endsWith(".class")) {
-                    String className = packageName + "." + file.getName().replace(".class", "");
-                    Class <?> clazz = Class.forName(className);
-
-                    if (clazz.isAnnotationPresent(CommandAnnotation.class)) {
-                        CommandAnnotation annotation = clazz.getAnnotation(CommandAnnotation.class);
-                        Command command = (Command) clazz
-                                .getDeclaredConstructor(String.class, String.class)
-                                .newInstance(annotation.name(), annotation.description());
-                        commands.put(annotation.name(), command);
-                    }
-                }
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
