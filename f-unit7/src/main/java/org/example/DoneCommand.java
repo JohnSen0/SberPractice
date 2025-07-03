1f8aac5accba1b08ad34f0eedeabec7994e91647
JohnSen0 <volars11@gmail.com>
2025-07-03 18:02:26 +0300

    Практика 8

--- f-unit7/src/main/java/org/example/DoneCommand.java
+++ f-unit7/src/main/java/org/example/DoneCommand.java
@@ -14,15 +14,11 @@ public class DoneCommand extends Command{
     public void execute(Scanner scanner) throws SQLException {
         System.out.print("Введите UID команды, чтобы пометить его как выполненное: ");
         String input = scanner.nextLine();
-        try {
-            int uid = Integer.parseInt(input);
-            String sql = "UPDATE todo_db SET status = TRUE WHERE uid = ? AND status = FALSE";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setInt(1, uid);
-                statement.executeUpdate();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: UID должен быть числом.");
+        int uid = Integer.parseInt(input);
+        String sql = "UPDATE todo_db SET status = TRUE WHERE uid = ? AND status = FALSE";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, uid);
+            statement.executeUpdate();
         }
     }
 }
